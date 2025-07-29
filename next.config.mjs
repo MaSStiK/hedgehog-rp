28063f039000aacc49c4e4987cb9b5a3867c282a
MaSStiK <mathew.ovchinnikov@gmail.com>
2025-07-29 18:59:00 +0200

    edit next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const path = require('path')
+
+const nextConfig = {
+  experimental: {
+    appDir: true,
+  },
+  webpack(config) {
+    config.resolve.alias['@'] = path.resolve(__dirname, 'src')
+    return config
+  }
+}
 
 export default nextConfig;
