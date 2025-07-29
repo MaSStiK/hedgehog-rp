41b4f2fcf6a3419c11ba8ba68044a8973918c091
MaSStiK <mathew.ovchinnikov@gmail.com>
2025-07-29 19:14:50 +0200

    rollback

--- next.config.js
+++
@@ -1,9 +0,0 @@
-const path = require('path');
-
-/** @type {import('next').NextConfig} */
-module.exports = {
-  webpack(config) {
-    config.resolve.alias['@'] = path.resolve(__dirname, 'src');
-    return config;
-  },
-};
