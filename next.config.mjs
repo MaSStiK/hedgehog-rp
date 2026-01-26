6aba4e8ec6b73c93c0e8b18c94ed5de65a0043b9
MaSStiK <mathew.ovchinnikov@gmail.com>
2026-01-26 03:35:26 +0100

    update next config

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    images: {
+        remotePatterns: [
+            {
+                protocol: "https",
+                hostname: "tv.hedgehog-rp.ru",
+                pathname: "/thumbnails/**",
+            }
+        ]
+    }
+};
 
 export default nextConfig;
