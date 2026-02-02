123b73d20b1dbf636d92e8be698be71982208c29
MaSStiK <mathew.ovchinnikov@gmail.com>
2026-02-02 23:49:29 +0100

    changed not-found page

--- src/app/not-found.jsx
+++ src/app/not-found.jsx
@@ -1,4 +1,4 @@
-import Link from "next/link"
+import Image from "next/image"
 import Button from "@/components/UI/Button/Button"
 import { Home } from "lucide-react"
 
@@ -7,15 +7,24 @@ import "@/app/styles/layout/not-found.css"
 export default function NotFound() {
     return (
         <div id="not-found">
-            <big>404 Not Found</big>
-            <h1>Страница не найдена!</h1>
-            <Link href="/">
-                <Button
-                    icon={Home}
-                    text={"На главную"}
-                    width100
+            <h1 className="visually-hidden">Страница не найдена</h1>
+            <div className="not-found__top flex-row">
+                <span className="fw-bold">4</span>
+                <Image
+                    src="/assets/not-found/not-found.webp"
+                    alt="hedgehog"
+                    width={128}
+                    height={128}
                 />
-            </Link>
+                <span className="fw-bold">4</span>
+            </div>
+            <p className="text-center fs-xlarge fw-bold">Упс...<br/>Мы не нашли такую страницу</p>
+            <Button
+                icon={Home}
+                text={"На главную"}
+                href="/"
+                width100
+            />
         </div>
     )
 }
